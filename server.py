d23aaff3a8d6c0c16a4ce26eb92a380adceeafe5
Guerby Benoit <guerby.benoit.1@ulaval.ca>
2025-03-25 20:12:40 -0400

    Remplacer app.py par server.py et ajout de README.md.

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
-from Flask import Flask, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def main():
+def index():  # put application's code here
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
